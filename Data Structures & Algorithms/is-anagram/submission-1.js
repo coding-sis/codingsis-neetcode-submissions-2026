5d5627dd47d7d691a9f0ac9bfcb5180f3a8a179c
coding-sis <100953927+coding-sis@users.noreply.github.com>
2026-04-14 06:17:45 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length) return false;
+        
+        const map = {};
+        for(let c of s) {
+            if(!map[c]) map[c] = 1;
+            else map[c] += 1;
+        }
+        for (let c of t) {
+            if(!map[c]) return false;
+            
+            map[c] -= 1;
+            if(!map[c]) delete map[c];
+        }
+        return Object.keys(map).length === 0;
+    }
+}
