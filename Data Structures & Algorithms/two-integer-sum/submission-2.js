2ef4f501d4b956ad171c29787df830a872414e22
coding-sis <100953927+coding-sis@users.noreply.github.com>
2026-04-13 21:07:20 -0700

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const map = new Map();
+        /* for(let i = 0; i < nums.length; i++) {
+            if(map.has(nums[i])) {
+                map.set(nums[i], [...map.get(nums[i]), i]);
+            } else {
+                map.set(nums[i], [i]);
+            }
+        }
+        
+        for(let i = 0; i < nums.length; i++) {
+            const diff = target - nums[i];
+            if (map.has(diff)) {
+                const indexes = map.get(diff);
+                const find = indexes.find(x => x != i);
+                if (find >= 0) {
+                    return [i, indexes.find(x => x != i)];
+                }
+            }
+        }
+        return [];*/
+
+        for(let i = 0; i < nums.length; i++) {
+            map.set(nums[i], i);
+        }
+
+        for(let i = 0; i < nums.length; i++) {
+            const diff = target - nums[i];
+            if(map.has(diff) && i != map.get(diff)) {
+                return [i, map.get(diff)];
+            }
+        }
+
+        return [];
+    }
+}
