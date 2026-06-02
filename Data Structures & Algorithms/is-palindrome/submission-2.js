22d61da303f2b2f26d4104beee82ab47fb07c855
coding-sis <100953927+coding-sis@users.noreply.github.com>
2026-06-02 06:31:39 -0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        if (!s || !s.length) return true;
+
+        let l = 0;
+        let r = s.trim().length-1;
+        while(l <= r) {
+            const cLeft = s[l].toLocaleLowerCase();
+            const cRight = s[r].toLocaleLowerCase();
+            
+            // check both cLeft and cRight are alphanumeric.
+            if(!/^[a-z0-9]$/i.test(cLeft)) {
+                l++;
+            } else if(!/^[a-z0-9]$/i.test(cRight)) {
+                r--;
+            }else if (cLeft === cRight) {
+                l++;
+                r--;
+            } else {
+                return false;
+            } 
+            
+        }
+        return true;
+    }
+}
