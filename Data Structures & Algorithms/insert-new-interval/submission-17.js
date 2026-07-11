ad8d622d7848c369c13df06cb6847641dbdea1da
coding-sis <100953927+coding-sis@users.noreply.github.com>
2026-07-10 21:31:29 -0700

    Add: insert-new-interval - submission-17

---
+++ Data Structures & Algorithms/insert-new-interval/submission-17.js
@@ -0,0 +1,55 @@
+// https://neetcode.io/problems/insert-new-interval/question?list=blind75
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @param {number[]} newInterval
+     * @return {number[][]}
+     */
+    insert(intervals, newInterval) {
+        if (!intervals.length) return [newInterval];
+
+        let res = [];
+        let [newS, newE] = [undefined, undefined]
+        let foundOverlap = false;
+        let inserted = false;
+        for(let i = 0; i < intervals.length; i++) {
+            let interval = intervals[i];
+
+            let overlap = this.isOverlapped(newInterval, interval);
+            while (overlap) {
+                foundOverlap = true;
+                let [s, e] = interval;
+                if(!newS) newS = Math.min(newInterval[0], s);
+                newE = Math.max(newInterval[1], e);
+                
+                interval = intervals[++i]
+                if(!interval) break;
+                overlap = this.isOverlapped(newInterval, interval);
+            }
+
+            if(!foundOverlap && interval) {
+                if(!inserted && interval[0] > newInterval[1]) {
+                    res.push(newInterval);
+                    inserted = true;
+                }
+                res.push(interval);
+            } else {
+                foundOverlap = false;
+                res.push([newS, newE]);
+                interval && res.push(interval);
+                inserted = true;
+            }
+        }
+        
+        if(!inserted) res.push(newInterval);
+        
+        return res;
+    }
+
+    isOverlapped(interval1, interval2) {
+        const [s1, e1] = interval1;
+        const [s2, e2] = interval2;
+
+        return !(e1 < s2 || e2 < s1) // not (not overlap)
+    }
+}
